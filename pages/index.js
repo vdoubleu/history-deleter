d1c7c901559e055712607a3f3dca112421d43497
Victor Wang <victorwang2001@gmail.com>
2022-03-16 12:45:58 -0400

    image works nowgit add --all

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 import NavBar from '../components/NavBar';
@@ -9,13 +10,20 @@ import { Container, Box, Typography, Button } from "@mui/material";
 
 import { useState } from 'react';
 
+const sites = ["google", "bing", "archive", "twitter", "reddit", "pornhub"];
+
 export default function Home() {
   const [openSiteSelectModal, setOpenSiteSelectModal] = useState(false);
+  const [currSite, setCurrSite] = useState("google");
   const router = useRouter();
 
   function handleSubmit(searchTarget) {
     console.log(searchTarget);
     router.push(`/search/${searchTarget}`);
+  }
+
+  function getImageFromSite(site) {
+    return `/images/${site}-logo.png`;
   }
 
   return (
@@ -30,7 +38,9 @@ export default function Home() {
 
         <Container maxWidth="md">
           <Box my={4} sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-          <Button onClick={() => setOpenSiteSelectModal(true)}> <Typography variant="h2"> Deleter </Typography> </Button>
+          <Button onClick={() => setOpenSiteSelectModal(true)}> 
+            <Image src={"/images/google-logo.png"} alt="logo" width={500} height={500} /> 
+          </Button>
             <Typography varient="h3" sx={{ m: 1 }}> Enter Keywords/Usernames that you have previously used </Typography>
             <SearchBar OnSubmit={handleSubmit} />
           </Box>
