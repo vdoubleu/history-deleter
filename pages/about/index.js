c5f1acfdac0d9581be7046730b37e5c34f4d92b1
Victor Wang <victorwang2001@gmail.com>
2022-04-02 08:42:13 -0400

    updated about us

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,10 +1,19 @@
+import { Typography, Container } from "@mui/material";
+
 import NavBar from '../../components/NavBar';
 
 export default function About() {
   return (
     <div>
       <NavBar />
-      about us page
+
+      <Container>
+      <Typography sx={{m: 2}} align={"center"} variant={"h2"}> About Us </Typography>
+      
+      <Typography sx={{}} align={"center"} variant={"h5"}>
+        This site helps those who wish to have traces of themselves removed from the internet. If there is content that you may deem undesirable, you can use our systems to help search and find the information and to remove it.
+      </Typography>
+      </Container>
     </div>
   );
 }
